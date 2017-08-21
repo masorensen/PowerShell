8d304e300696db0c731b3e6b373913b426f602e4
Mike Sorensen <masorensen@users.noreply.github.com>
2017-08-21 11:58:34 -0500

    Add files via upload

---
+++ Publish-DSCResourceRemotely.ps1
@@ -0,0 +1,18 @@
+﻿function Publish-DSCResourceRemotely {
+Param(
+    [string[]]$Module,
+    [string]$ComputerName
+)
+
+foreach ($ModuleName in $Module){
+    
+    $ModuleVersion = (Get-Module $ModuleName -ListAvailable).Version
+    $ModulePath = (Get-Module $ModuleName -ListAvailable | Select-Object Path) | Split-Path | Split-Path
+    $DestinationZipPath = "\\$ComputerName\c`$\Program Files\WindowsPowerShell\DscService\Modules\$($ModuleName)_$($ModuleVersion).zip"
+    
+    Compress-Archive -Update -Path "$ModulePath\*" -DestinationPath $DestinationZipPath
+    
+    New-DscChecksum $DestinationZipPath
+}
+
+}
