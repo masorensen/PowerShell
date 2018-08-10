8280ae222a47a98c60f3c307c471af37fecdec5f
Mike Sorensen <masorensen@users.noreply.github.com>
2018-08-10 13:36:42 -0500

    Add files via upload

---
+++ DSC/Publish-DSCResource.ps1
@@ -0,0 +1,15 @@
+Param
+(
+    [string[]]$Module,
+    [string]$ComputerName
+)
+foreach ($ModuleName in $Module)
+{    
+    $ModuleVersion = (Get-Module $ModuleName -ListAvailable).Version
+    $ModulePath = (Get-Module $ModuleName -ListAvailable | Select-Object Path) | Split-Path | Split-Path
+    $DestinationZipPath = "\\$ComputerName\c`$\Program Files\WindowsPowerShell\DscService\Modules\$($ModuleName)_$($ModuleVersion).zip"
+
+    Compress-Archive -Update -Path "$ModulePath\*" -DestinationPath $DestinationZipPath
+
+    New-DscChecksum $DestinationZipPath
+}
