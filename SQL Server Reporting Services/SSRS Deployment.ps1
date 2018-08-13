98bcf381fe0752cd762eee6f1b8758cf309873c3
Mike Sorensen <masorensen@users.noreply.github.com>
2018-08-13 14:54:36 -0500

    Update SSRS Deployment.ps1

--- SQL Server Reporting Services/SSRS Deployment.ps1
+++ SQL Server Reporting Services/SSRS Deployment.ps1
@@ -1,26 +1,41 @@
 param(
+	#DNS name of the report server
 	[string]$ServerName,
-	[string]$CSVPath,
+	#Root path where the .rdl report files are stored
 	[string]$SourcePath,
+	#Name of the reporting instance
 	[string]$InstanceName,
+	#Name of the database holding the reporting data
 	[string]$DatabaseName,
+	#Name of the database server holding the reporting data database
 	[string]$DatabaseServer,
-	[string]$DatasourceName,
-	[string]$User,
-	[securestring]$Pass,
+	#Name of the SSRS Datasource
+	[string]$DatasourceName,	
+	#Deploy folders in the reporting instance?
 	[switch]$DeployFolders,
-	[switch]$CreateDatasource
+	#Create the datasource?
+	[Parameter(ParameterSetName="CreateDatasource", Mandatory=$false)][switch]$CreateDatasource,
+	#User that will connect to the reporting database
+	[Parameter(ParameterSetName="CreateDatasource", Mandatory=$true)][string]$User,
+	#Password
+	[Parameter(ParameterSetName="CreateDatasource", Mandatory=$true)][securestring]$Pass,
+	[Parameter(ParameterSetName="Install", Mandatory=$false)][switch]$Install,
+	[Parameter(ParameterSetName="Install", Mandatory=$true)][string]$InstallerPath
 )
 
-Write-Host "Populating a list of the reports to upload..."
+#Instantiate the array that will hold the report metadata
 $ReportArray = @()
+#Loop through the root directory provided in the parameters
 foreach($Report in Get-ChildItem -af -Path $SourcePath -Recurse) {
 	$Item = $Report
 	$Name = [io.path]::GetFileNameWithoutExtension($Item)          #Strip the extension off of the filename - name of the item.
 	$Path = Split-Path $Report.FullName
+	#Decide which type of path was provided - drive letter or UNC path
 	if($Path -like "*:*") {
+		#Drive letter
 		$Path = $Path.Split("\")
 		$Parent = ""
+		#Build the parent path by combining each leaf of the rdl path
 		for ($i = 1; $i -lt $Path.Count; $i++) {
 			if($i -eq $Path.Count -1) {
 				$Parent += $Path[$i]
@@ -29,9 +44,11 @@ foreach($Report in Get-ChildItem -af -Path $SourcePath -Recurse) {
 			}	
 		}
 	} elseif($Path -like "\\*") {
+		#UNC Path
 		$Path = $Path.Replace("\\", "")
 		$Path = $Path.Split("\")
 		$Parent = ""
+		#Build the Parent path by combining each leaf of the rdl path
 		for ($i = 1; $i -lt $Path.Count; $i++) {
 			if($i -eq $Path.Count) {
 				$Parent += $Path[$i]
@@ -40,44 +57,47 @@ foreach($Report in Get-ChildItem -af -Path $SourcePath -Recurse) {
 			}			
 		}
 	} elseif($Path -match "^$") {
+		#We weren't able to parse the path's parent, so it's parent is the root of the report server or '/'
 		$Parent = "/"
-	} else {
-		Write-Host "Please enter the root path to the reports using either UNC path or drive letter path"
-		return
 	}
-	$Row = "" | Select-Object RDLFileName, ReportServerName, ReportBaseFolder, ReportName, ParentFolder
+
+	#Create a custom array variable to hold our metadata
+	$Row = "" | Select-Object RDLFileName, ReportServerName, ReportBaseFolder, ReportName, ParentFolder, FullPath
+	$Report = "" | Select-Object RDLFileName, ReportServerName, ReportBaseFolder, ReportName, ParentFolder, FullPath
+	#Populate the metadata into the custom array variable
 	$Row.RDLFileName = $Item
 	$Row.ReportServerName = $InstanceName
 	$Row.ReportBaseFolder = "/"
 	$Row.ReportName = $Name
 	$Row.ParentFolder = $Parent
+	$Row.FullPath = $Report.FullName
+	#Add the custom array variable to the $ReportArray variable
 	$ReportArray += $Row
 }
 	    
 function New-SSRSFolder
 (
-		[string]$FolderName,		
-		[string]$Parent	
+	[string]$FolderName,		
+	[string]$Parent	
 )
 {
-	Write-Host "Creating $FolderName folder..."
-	
+	#Declare the Url to the ReportService2010.asmx page	
 	$ReportServerUri = "http://$ServerName/ReportServer_$InstanceName/ReportService2010.asmx"
+	#Create a new proxy from the $ReportServerUri variable. Use default credentials
 	$Proxy = New-WebServiceProxy -Uri $ReportServerUri -UseDefaultCredential
 	
-	$Type = $Proxy.GetType().Namespace #Giving the variable a specific type, this is necessary due to the implied namespace and datatype confusion of 
-										#Microsoft.ReportingServices.WebServer.ReportingService2010
-	$Datatype = ($Type + '.Property')  #Cast that variable in to what we need
-			
-	$Property =New-Object ($Datatype); #Create the property variable with the datatype we need and set a couple properties for later
+	#Explicit cast to the namespace type of the $proxy object(compatability reasons)
+	$Type = $Proxy.GetType().Namespace
+	$Datatype = ($Type + '.Property')			
+	$Property = New-Object ($Datatype)
+	#Assign values to the object properties
 	$Property.Name = $FolderName
-	$Property.Value = $FolderName
-			
+	$Property.Value = $FolderName			
 	$NumProperties = 1
 	$Properties = New-Object ($Datatype + '[]')$NumProperties 
 	$Properties[0] = $Property;
-		
-	$Proxy.CreateFolder($FolderName, $Parent, $Properties);  #Call the CreateFolder method from the ReportService2010 namespace	    
+	#Run the CreateFolder function from the ReportService2010.asmx namespace using the property values assigned earlier
+	$Proxy.CreateFolder($FolderName, $Parent, $Properties)
 }
 	
 function Publish-SSRS
@@ -85,18 +105,20 @@ function Publish-SSRS
 	[string[]]$ReportArray
 )
 {
+	#Loop through each report in the report array passed to the function
 	foreach($Report in $ReportArray) {
-		$ReportServiceURI = "http://$ServerName/ReportServer_$InstanceName/ReportService2010.asmx"   
-		$RDLFileName = $SourcePath + $Report.ParentFolder + "\" + $Report.RDLFileName
-		$FinalPath = $Report.ReportBaseFolder + $Report.ParentFolder
+		#Declare the Url to the ReportService2010.asmx page
+		$ReportServiceURI = "http://$ServerName/ReportServer_$InstanceName/ReportService2010.asmx"
 	    #Write some updates to the user
-	    Write-Host "----------------------------------------------------------------------------------------------------------------------------------------"
-	    Write-Host "Deploying" $RDLFileName 
-	    Write-Host " to" $FinalPath "as" $_.ReportName		
-		
-	    $SSRSproxy = New-WebServiceProxy -uri $ReportServiceURI -UseDefaultCredential 
-	    $RDLStream = Get-Content $RDLFileName -Encoding byte
-	    $Warnings =@();
+	    Write-Host "Deploying" $Report.RDLFileName
+	    Write-Host "to" $Report.ParentFolder "as" $Report.ReportName	
+		#Instantiate a new proxy for interacting with the web service
+		$SSRSproxy = New-WebServiceProxy -Uri $ReportServiceURI -UseDefaultCredential
+		#Convert the .rdl file to a file stream that can be parsed
+		$RDLStream = Get-Content $RDLFileName -Encoding byte
+		#Instantiate an array to hold any warnings that are generated by the method CreateCatalogItem
+		$Warnings = @()
+		#Call the CreateCatalogItem method that is a part of the ReportService2010 namespace
 	    $SSRSproxy.CreateCatalogItem("Report", $Report.Name, $Path, $true, $RDLStream, $null, [ref]$Warnings)
 	
 	    #Send any warnings to the user
@@ -106,32 +128,38 @@ function Publish-SSRS
 	        }
 	    }
 	}
+	Set-Datasource
 }
 
 function Set-DataSource
-(
-	[string]$ReportFolderPath
-)
 {
-	$TargetDataSourcePath = "/DataSources/$DatasourceName"
-	$TargetDataSourceName = "$DatasourceName"
-	
-	$Url = "http://$($ServerName)/ReportServer_$($InstanceName)/ReportService2010.asmx"	
-	$SSRS = New-WebServiceProxy -Uri $Url -UseDefaultCredential	
+	#Instantiate a new proxy to interact with the web service
+	$Url = "http://$ServerName/ReportServer_$InstanceName/ReportService2010.asmx"	
+	$SSRS = New-WebServiceProxy -Uri $Url -UseDefaultCredential
+	#Load all the child items to '/' in an array
 	$CatalogItems = $SSRS.ListChildren("/", $true)
-
+	#Loop through each catalog item
 	foreach($CatalogItem in $CatalogItems) {
+		#Check if the CatalogItem is a report
 		if($CatalogItem.TypeName -eq "Report") {
+			Write-Host "Setting datasource for " + $CatalogItem.Path
+			#The CatalogItem is a report
 			$ReportPath = $CatalogItem.Path
+			#Get all the datasources associated with the report
 			$Datasources = $SSRS.GetItemDataSources($ReportPath)
+			#Loop through each datasource associated with the report
 			foreach($Datasource in $Datasources) {
+				#Explicit cast to the namespace type of the $proxy object(compatability reasons)
 				$ProxyNamespace = $Datasource.GetType().Namespace
+				#Create a new datasource object
 				$TargetDataSource = New-Object ("$ProxyNamespace.DataSource")
+				#Assign values to the properties of the new datasource
 				$TargetDataSource.Name = $TargetDataSourceName
 				$TargetDataSource.Item = New-Object ("$ProxyNamespace.DataSourceReference")
 				$TargetDataSource.Item.Reference = $TargetDataSourcePath
-				$Datasource.item = $TargetDataSource.Item					
-				$SSRS.SetItemDataSources($ReportPath, $Datasource) #Set the object to the new data source using the SetItemDataSources method of the ReportingServices2010 namespace
+				$Datasource.Item = $TargetDataSource.Item
+				#Call the SetItemDataSources method that is a part of the ReportService2010 namespace
+				$SSRS.SetItemDataSources($ReportPath, $Datasource)
 			}
 		}
 	}
@@ -142,31 +170,40 @@ function New-DataSource
 )
 {
 	Write-Host "Creating Datasource..."
-	$ReportServerUri  = "http://$($ServerName)/ReportServer_$($InstanceName)/ReportService2010.asmx"
+	#Declare the Url to the ReportService2010 web service
+	$ReportServerUri  = "http://$ServerName/ReportServer_$InstanceName/ReportService2010.asmx"
+	#Instantiate a new proxy to interact with the ReportService2010 web service
 	$Proxy = New-WebServiceProxy -Uri $ReportServerUri -UseDefaultCredential
-	$Type = $Proxy.GetType().Namespace #Explicit cast to the namespace type of the $proxy object(compatability reasons)
-
-	#create a DatasourceDefinition and set some properties of the new object
+	#Explicit cast to the namespace type of the $proxy object(compatability reasons)
+	$Type = $Proxy.GetType().Namespace
+	#Create a DatasourceDefinition and set the property values
 	$DatasourceDefinitionType = ($type + '.DatasourceDefinition')
 	$DatasourceDefinition = New-Object($DatasourceDefinitionType)
 	$DatasourceDefinition.CredentialRetrieval = "Store"
 	$DatasourceDefinition.UserName = $User
 	$DatasourceDefinition.Password = $Pass
-	$DatasourceDefinition.ConnectString = "Data Source=$($DatabaseServer);Initial Catalog=$($DatabaseName)"
+	$DatasourceDefinition.ConnectString = "Data Source=$DatabaseServer;Initial Catalog=$DatabaseName"
 	$DatasourceDefinition.extension = "SQL"
 	$DatasourceDefinition.enabled = $true
 	$DatasourceDefinition.Prompt = $null
 	$DatasourceDefinition.WindowsCredentials = $false
 
-	$ParentFolder = "/DataSources"        #Parent folder
-	$Overwrite = $true #Overwrite the datasource if it exists?             
+	$ParentFolder = "/DataSources"
+	$Overwrite = $true
 
 	#Create the datasource from the parameters set above
 	$Proxy.CreateDataSource($DatasourceName, $ParentFolder, $Overwrite, $DatasourceDefinition, $null)
 }
 
-if($DeployFolders)
-{	
+<#
+if($Install) {
+	$SQLInstallerString = $InstallerPath + "\" + "Setup.exe /qs /Action=Install UpdateEnabled=0 /InstanceName='$InstanceName' /IACCEPTSQLSERVERLICENSETERMS  /Action=Install /FEATURES=RS /RSINSTALLMODE=DEFAULTNATIVEMODE /RSSVCSTARTUPTYPE='Automatic'"
+	& $SQLInstallerString
+
+}
+#>
+
+if($DeployFolders) {	
 	foreach($Item in Get-ChildItem -Path $SourcePath -Recurse) {
 		if($Item.Attributes -like "*Directory*") {
 			$RelativePath = $Item.FullName.Replace($SourcePath, '')
@@ -182,22 +219,14 @@ if($DeployFolders)
 			New-SSRSFolder -FolderName $FolderName -Parent $ParentFolder
 		}
 	}
+	New-SSRSFolder -FolderName "DataSources" -Parent "/"
 }
 
-if($CreateDatasource)
-{
-	New-Datasource -DatasourceName "Datasource"
+if($CreateDatasource) {
+	New-Datasource -DatasourceName "DataSource"
 }
 
-Publish-SSRS -InstanceName $InstanceName -ReportServer $ServerName 
-Set-DataSource -InstanceName $InstanceName -ReportServer $ServerName -ReportFolderPath "/FHNetReports/System Reports"
-Set-DataSource -InstanceName $InstanceName -ReportServer $ServerName -ReportFolderPath "/FHNetReports/System Reports/EMS"
-Set-DataSource -InstanceName $InstanceName -ReportServer $ServerName -ReportFolderPath "/FHNetReports/System Reports/Incident"
+#Deploy the reports
+Publish-SSRS -ReportArray $ReportArray
 
-
-
-
-#Use this to install a new RS instance - modify the instance name.
-#Y:\InstallMedia\Microsoft\SQL\SQL2012EE\SQLServer2012EE\Setup.exe /qs /Action=Install UpdateEnabled=0 /InstanceName="SSRSMDFREDERICK" /IACCEPTSQLSERVERLICENSETERMS  /Action=Install /FEATURES=RS /RSINSTALLMODE=DEFAULTNATIVEMODE /RSSVCSTARTUPTYPE="Automatic"
-Write-Host -ForegroundColor Yellow "Press any key to exit ..."
-$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Read-Host "Press ENTER to exit ..."
